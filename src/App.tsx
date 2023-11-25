e1c9e5559819e31471f3c7518144171ed929fb91
Jumah Muhamad <jumahtech@gmail.com>
2023-11-25 15:44:52 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./components/Navbar"
+import './components/NavbarStyle.css'
+
+function App () {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/message.tsx
@@ -0,0 +1,7 @@
+
+function message () {
+  return
+    <h1>Message</h1>
+}
+
+export default message
