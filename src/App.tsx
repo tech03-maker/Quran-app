c56542f0183df2d95cb8f0d169edae25bd74a8a6
Jumah Muhamad <jumahtech@gmail.com>
2023-11-29 15:30:37 +0100

    file import edit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import "./components/NavbarStyle.css";
 import "./components/HomePage.css"
 import { library } from "@fortawesome/fontawesome-svg-core";
-import HomePage from "./components/homepage";
+import HomePage from "./components/HomePage";
 import { faBars, faPhone, faLanguage } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faBars, faPhone, faLanguage);
